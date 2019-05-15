f781e0a7977a91baef72b72d45ed32dbf36687c9
Julio Schilders <julio.schilders@gmail.com>
2019-05-16 00:35:18 +0200

    Updates

---
+++ precache-manifest.df51d0d77d8d7992b9eaaf785c1a21ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13685405092d2b9cc51eb0ab44c5d741",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "c863fc28a96291607ebc",
+    "url": "/react-todo-list/static/css/main.c7b78092.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-todo-list/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c863fc28a96291607ebc",
+    "url": "/react-todo-list/static/js/main.4903b300.chunk.js"
+  },
+  {
+    "revision": "89737d6b5129f89a8a0b",
+    "url": "/react-todo-list/static/js/runtime~main.eb2f80b6.js"
+  }
+]);
